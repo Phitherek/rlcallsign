6242086ecd84b51ab265b78cef80947177c55dac
Phitherek_ <phitherek@gmail.com>
2015-09-14 22:36:23 +0200

    Added update_current_qth to API.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -25,4 +25,31 @@ class ApiController < ApplicationController
             render json: {info: {callsign: @user.callsign, name: @user.callsign_info.name, stationary_qth: @user.callsign_info.stationary_qth, stationary_qth_locator: @user.callsign_info.stationary_qth_locator, current_qth: (@user.callsign_info.current_qth.nil? || @user.callsign_info.current_qth.empty?) && (@user.callsign_info.current_qth_locator.nil? || @user.callsign_info.current_qth_locator.empty?) ? @user.callsign_info.stationary_qth : @user.callsign_info.current_qth, current_qth_locator: (@user.callsign_info.current_qth.nil? || @user.callsign_info.current_qth.empty?) && (@user.callsign_info.current_qth_locator.nil? || @user.callsign_info.current_qth_locator.empty?) ? @user.callsign_info.stationary_qth_locator : @user.callsign_info.current_qth_locator}}
         end
     end
+
+    def update_current_qth
+        if params[:token].nil? || params[:token].empty?
+            render json: {error: "rlauthtokenrequired"}
+        else
+            res = HTTParty.post('https://rlauth.deira.phitherek.me/altapi/user_data', token: params[:token])
+            res = JSON.parse(res.body)
+            if !res['error'].nil?
+                render json: {error: "rlauth_#{res['error']}"}
+            else
+                @user = RemoteUser.find_by_callsign(res['user']['callsign'])
+                if @user.nil?
+                    render json: {error: "unauthorized"}
+                elsif @user.callsign_info.nil?
+                    render json: {error: "nocallsigninfo"}
+                else
+                    @user.callsign_info.current_qth = params[:current_qth]
+                    @user.callsign_info.current_qth_locator = params[:current_qth_locator]
+                    if @user.callsign_info.save
+                        render json: {success: "success"}
+                    else
+                        render json: {error: "validation"}
+                    end
+                end
+            end
+        end
+    end
 end
